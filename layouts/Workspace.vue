27599c7f734c641b991b8abffcd552fcf756706a
Luka Grabnar <grabnar.luka@gmail.com>
2024-06-24 17:53:37 +0200

    load data in a layout

--- layouts/Workspace.vue
+++ layouts/Workspace.vue
@@ -1,13 +1,51 @@
 <template>
 	<LoggedInHeaderShad />
 	<div class="flex flex-row flex-1 relative overflow-y-auto h-[100vh]">
-		<WorkspaceSidebar />
+		<WorkspaceSidebar
+			:currentWorkspaceData="workspaceData"
+			v-if="workspaceData"
+		/>
 		<div class="w-full m-5">
+			<WorkspaceInfoHeader
+				:workspaceData="workspaceData"
+				v-if="
+					route.fullPath.includes('members') ||
+					route.fullPath.includes('account') ||
+					route.fullPath == `/w/${workspaceData?.id_str}`
+				"
+			/>
 			<slot />
 		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
-	definePageMeta({ middleware: ["auth"] });
+	import type { Workspace } from "~/lib/types";
+
+	const route = useRoute();
+	const workspaceData = ref<Workspace | undefined>();
+	const myWorkspaceStore = useMyWorkspaceStore();
+
+	onMounted(async () => {
+		if (route.params.wid != undefined) {
+			if (myWorkspaceStore.$state.workspaces.length == 0) {
+				await myWorkspaceStore.loadWorkspace(route.params.wid as string);
+			}
+			workspaceData.value = myWorkspaceStore.getWorkspace(
+				route.params.wid as string
+			);
+		}
+		if (route.params.bid != undefined) {
+			if (myWorkspaceStore.$state.workspaces.length == 0) {
+				await myWorkspaceStore.loadWorkspace(route.params.wid as string);
+			}
+
+			const board = myWorkspaceStore.getBoardById(route.params.bid as string);
+			workspaceData.value = myWorkspaceStore.getWorkspace(
+				board.workspace_id_str
+			);
+		}
+
+		console.log(route);
+	});
 </script>

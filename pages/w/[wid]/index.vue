3fac1bf82f2af86c030808d4bd6a4ae5c2a7b9db
Luka Grabnar <grabnar.luka@gmail.com>
2024-06-25 18:58:14 +0200

    filter select

--- pages/w/[wid]/index.vue
+++ pages/w/[wid]/index.vue
@@ -9,55 +9,98 @@
 
 		<!--FILTERS-->
 		<div
-			class="flex flex-row flex-wrap justify-between max-w-[1250px] pt-[20px] pr-0 pb-[8px] pl-0 relative"
+			class="flex flex-row flex-wrap justify-between pt-[20px] pr-0 pb-[8px] pl-0 relative w-full"
 		>
 			<div class="flex flex-row">
-				<Select
-					defaultValue="mostRecent"
-					v-model="sortBy"
-				>
-					<SelectTrigger>
-						<SelectValue placeholder="Most recent active" />
-					</SelectTrigger>
-					<SelectContent>
-						<SelectGroup>
-							<SelectItem value="mostRecent"> Most recent active </SelectItem>
-							<SelectItem value="leastRecent"> Least recent active </SelectItem>
-							<SelectItem value="az"> Alphabetically A-Z </SelectItem>
-							<SelectItem value="za"> Alphabetically Z-A </SelectItem>
-						</SelectGroup>
-					</SelectContent>
-				</Select>
+				<div class="w-[200px]">
+					<p class="text-gray-600 text-[12px] font-bold mb-1">Sort by</p>
+					<Select
+						defaultValue="mostRecent"
+						v-model="sortBy"
+					>
+						<SelectTrigger>
+							<SelectValue placeholder="Most recent active" />
+						</SelectTrigger>
+						<SelectContent class="rounded-none py-2">
+							<SelectGroup>
+								<SelectItemTheme value="mostRecent">
+									Most recent active
+								</SelectItemTheme>
+								<SelectItemTheme value="leastRecent">
+									Least recent active
+								</SelectItemTheme>
+								<SelectItemTheme value="az">
+									Alphabetically A-Z
+								</SelectItemTheme>
+								<SelectItemTheme value="za">
+									Alphabetically Z-A
+								</SelectItemTheme>
+							</SelectGroup>
+						</SelectContent>
+					</Select>
+				</div>
+				<div class="pl-2 w-[200px]">
+					<p class="text-gray-600 text-[12px] font-bold mb-1">Filter by</p>
+					<PopoverRoot>
+						<PopoverTrigger
+							class="border text-sm py-2 px-3 rounded-md w-full flex justify-between items-center"
+							>Choose a collection
+							<Icon
+								name="tabler:chevron-down"
+								class="w-[18px] h-[18px] text-gray-600 hover:!text-gray-600"
+						/></PopoverTrigger>
+						<PopoverPortal>
+							<PopoverContent
+								side="bottom"
+								align="start"
+								class="w-[304px] bg-white py-2 px-2 rounded-lg shadow-lg"
+							>
+								<div class="flex justify-between">
+									<div class="w-[32px]" />
+									<h1 class="font-semibold text-sm">Collections</h1>
+									<PopoverClose class="w-[32px] hoverButton">x</PopoverClose>
+								</div>
 
-				<Select>
-					<SelectTrigger>
-						<SelectValue placeholder="Choose a collection" />
-					</SelectTrigger>
-					<SelectContent>
-						<SelectGroup>
-							<SelectLabel>Fruits</SelectLabel>
-							<SelectItem value="apple"> Apple </SelectItem>
-						</SelectGroup>
-					</SelectContent>
-				</Select>
+								<div class="max-h-[795px] mt-4">
+									<div class="flex flex-col items-center text-center">
+										<p class="mb-4 font-semibold text-[16px]">
+											Organize your boards with collections
+										</p>
+										<p class="mb-4 text-sm">
+											Upgrade to Premium to group your boards by department,
+											topic, team and more
+										</p>
+										<Button class="bg-[#DFD8FD] text-black h-9 rounded-sm">
+											Try it free for 14 days
+										</Button>
+									</div>
+								</div>
+							</PopoverContent>
+						</PopoverPortal>
+					</PopoverRoot>
+				</div>
 			</div>
 
-			<div class="relative flex flex-col min-w-60">
-				<Label for="searchBoards">Search</Label>
-				<Input
-					id="searchBoards"
-					type="search"
-					placeholder="Search boards"
-				/>
-				<span
-					class="absolute inset-y-0 start-0 flex items-center justify-center px-2"
-				>
-					<Icon
-						name="tabler:search"
-						:ssr="true"
-						class="text-black dark:text-slate-400"
+			<div>
+				<p class="text-gray-600 text-[12px] font-bold mb-1">Search</p>
+				<div class="relative flex flex-col min-w-60">
+					<Input
+						id="searchBoards"
+						type="search"
+						placeholder="Search boards"
+						class="pl-10 rounded-sm"
+						v-model="searchValue"
 					/>
-				</span>
+					<span
+						class="absolute start-0 inset-y-0 flex items-center justify-center px-2"
+					>
+						<Icon
+							name="tabler:search"
+							:ssr="true"
+							class="text-slate-600 dark:text-slate-400"
+						/>
+					</span>
+				</div>
 			</div>
 		</div>
 
@@ -84,6 +127,15 @@
 
 <script lang="ts" setup>
 	import type { Board, Workspace } from "~/lib/types";
+	import {
+		PopoverAnchor,
+		PopoverArrow,
+		PopoverClose,
+		PopoverContent,
+		PopoverPortal,
+		PopoverRoot,
+		PopoverTrigger,
+	} from "radix-vue";
 	definePageMeta({
 		layout: "workspace",
 		middleware: ["auth"],
@@ -94,27 +146,38 @@
 	const boards = ref<Board[]>([]);
 	const sortBy = ref("az");
 	const myWorkspaceStore = useMyWorkspaceStore();
+	const searchValue = ref("");
 
 	const sortedBoards = computed(() => {
 		const sorted = [...boards.value];
 		switch (sortBy.value) {
 			case "mostRecent":
-				return sorted.sort(
+				sorted.sort(
 					(a, b) =>
 						new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
 				);
+				break;
 			case "leastRecent":
-				return sorted.sort(
+				sorted.sort(
 					(a, b) =>
 						new Date(a.updated_at).getTime() - new Date(b.updated_at).getTime()
 				);
+				break;
 			case "az":
-				return sorted.sort((a, b) => a.title.localeCompare(b.title));
+				sorted.sort((a, b) => a.title.localeCompare(b.title));
+				break;
 			case "za":
-				return sorted.sort((a, b) => b.title.localeCompare(a.title));
-			default:
-				return sorted;
+				sorted.sort((a, b) => b.title.localeCompare(a.title));
+				break;
 		}
+
+		if (searchValue.value) {
+			return sorted.filter((board) =>
+				board.title.toLowerCase().includes(searchValue.value.toLowerCase())
+			);
+		}
+
+		return sorted;
 	});
 
 	onMounted(() => {
--- shadComponents/ui/select/SelectItemTheme.vue
+++ shadComponents/ui/select/SelectItemTheme.vue
@@ -28,7 +28,7 @@
 		v-bind="forwardedProps"
 		:class="
 			cn(
-				'data-[state=checked]:text-primary relative flex w-full cursor-default select-none items-center rounded-sm !py-1.5 !pl-8 !pr-2 text-sm outline-none focus:bg-accent focus:bg-blue-400 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[state=checked]:bg-lightBlueBg data-[state=checked]:text-blue-500 data-[disabled]:opacity-50 dark:data-[state=checked]:bg-darkBlueBg',
+				'relative flex w-full cursor-default select-none items-center rounded-none py-3 pl-8 pr-2 text-sm outline-none hover:bg-accent hover:shadow-blueLeft focus:text-accent-foreground data-[disabled]:pointer-events-none data-[state=checked]:bg-blue-200/40 data-[state=checked]:text-blue-500 focus:data-[state=checked]:bg-blue-200/90 data-[state=checked]:shadow-blueLeft data-[disabled]:opacity-50',
 				props.class
 			)
 		"

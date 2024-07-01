dfd77d1199a84afb40c24a508879417ee1334c73
Luka Grabnar <grabnar.luka@gmail.com>
2024-07-02 00:23:39 +0200

    cleanup

--- plugins/workspace.ts
+++ plugins/workspace.ts
@@ -1,6 +1,5 @@
 export default defineNuxtPlugin(async () => {
     const { user } = useMyUserStore();
-    const { getWorkspace, setCurrentWorkspace, getBoardById, setCurrentBoard } = useMyWorkspaceStore()
     const route = useRoute();
     if (!user) { return };
     const myWorkspaceStore = useMyWorkspaceStore();
@@ -10,11 +9,9 @@ export default defineNuxtPlugin(async () => {
 
     const handleRouteChange = async (routeParams: any) => {
         if (routeParams.wid !== undefined) {
-            console.log(routeParams.wid)
             const workspace = myWorkspaceStore.getWorkspace(routeParams.wid as string)
             myWorkspaceStore.setCurrentWorkspace(workspace)
         } else if (routeParams.bid !== undefined) {
-            console.log(routeParams.bid)
             const board = myWorkspaceStore.getBoardById(routeParams.bid as string)
             const workspace = myWorkspaceStore.getWorkspace(board.workspace_id_str)
             myWorkspaceStore.setCurrentWorkspace(workspace)

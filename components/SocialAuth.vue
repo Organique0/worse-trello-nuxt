7e95ae663d451c3c208d61b5f396548ab21431c7
Luka Grabnar <grabnar.luka@gmail.com>
2024-07-03 21:43:10 +0200

    change url

--- components/SocialAuth.vue
+++ components/SocialAuth.vue
@@ -1,22 +1,28 @@
 <template>
-  <div>
-    <UButton color="white" class="mb-4 h-10" block @click="onSubmit">
-      <LogosGithubIcon v-if="props.for === 'github'" />
-      <LogosGoogleIcon v-if="props.for === 'google'" />
-      {{ props.for.charAt(0).toUpperCase() + props.for.slice(1) }}
-    </UButton>
-  </div>
+	<div>
+		<UButton
+			color="white"
+			class="mb-4 h-10"
+			block
+			@click="onSubmit"
+		>
+			<LogosGithubIcon v-if="props.for === 'github'" />
+			<LogosGoogleIcon v-if="props.for === 'google'" />
+			{{ props.for.charAt(0).toUpperCase() + props.for.slice(1) }}
+		</UButton>
+	</div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps({
-  for: {
-    type: String,
-    required: true,
-  },
-});
+	const { backendUrl } = useRuntimeConfig().public;
+	const props = defineProps({
+		for: {
+			type: String,
+			required: true,
+		},
+	});
 
-const onSubmit = async () => {
-  window.location.href = `http://localhost:8000/login/${props.for}`;
-};
+	const onSubmit = async () => {
+		window.location.href = `${backendUrl}/login/${props.for}`;
+	};
 </script>

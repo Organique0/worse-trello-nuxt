db4f0877c27a1ed4e345ce6719790be15b63795d
Luka Grabnar <grabnar.luka@gmail.com>
2024-07-04 00:01:23 +0200

    Revert "login"
    This reverts commit c4439fe0177aeff0d7f2d9f7e81b27ce042568cf.

--- components/SocialAuth.vue
+++ components/SocialAuth.vue
@@ -23,6 +23,14 @@
 	});
 
 	const onSubmit = async () => {
-		window.location.href = `${backendUrl}/login/${props.for}`;
+		await initiateOAuthFlow();
+	};
+
+	const initiateOAuthFlow = async () => {
+		const redirectUri = encodeURIComponent(
+			`${window.location.origin}/auth/google/callback`
+		);
+		const authUrl = `${backendUrl}/auth/google?redirect_uri=${redirectUri}`;
+		window.location.href = authUrl;
 	};
 </script>

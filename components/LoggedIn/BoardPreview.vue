164c8771ba5a98615cbc77de33ef52fe19046a28
Luka Grabnar <grabnar.luka@gmail.com>
2024-07-09 22:29:47 +0200

    better dark mode board preview

--- components/LoggedIn/BoardPreview.vue
+++ components/LoggedIn/BoardPreview.vue
@@ -1,28 +1,30 @@
 <template>
-	<NuxtLink
-		class="group relative"
-		:to="resolvedUrl"
-	>
-		<div
-			:style="giveBackgroundImage(src)"
-			class="h-[100px] rounded-sm w-full cursor-pointer group-hover:brightness-90 bg-cover bg-[50%]"
-			:class="class"
-		/>
-		<p
-			class="text-medium absolute left-3 top-2 w-[170px] cursor-pointer select-none font-bold text-white"
+	<div class="relative overflow-hidden">
+		<NuxtLink
+			class="group"
+			:to="resolvedUrl"
 		>
-			{{ board.title }}
-		</p>
+			<div
+				:style="giveBackgroundImage(src)"
+				class="h-[100px] rounded-sm w-full cursor-pointer group-hover:brightness-50 brightness-75 bg-cover bg-[50%]"
+				:class="class"
+			/>
+			<p
+				class="text-medium absolute left-3 top-2 w-[170px] cursor-pointer select-none font-bold text-white"
+			>
+				{{ board.title }}
+			</p>
 
-		<LogoFavorite
-			:class="
-				board.is_favorited
-					? 'fill-yellow-600 stroke-yellow-600 stroke-2 hover:fill-none absolute bottom-2 right-2'
-					: 'ease translate-x-[150%] transform fill-transparent stroke-white stroke-2 transition-transform duration-300 group-hover:translate-x-0 absolute bottom-2 right-2'
-			"
-			@click.stop="myWorkspaceStore.favorite(board.id_str)"
-		/>
-	</NuxtLink>
+			<LogoFavorite
+				:class="
+					board.is_favorited
+						? 'fill-yellow-600 stroke-yellow-600 dark:fill-yellow-500 dark:stroke-yellow-500 stroke-2 hover:fill-none absolute bottom-2 right-2'
+						: 'ease translate-x-[150%] transform fill-transparent stroke-white stroke-2 transition-transform duration-300 group-hover:translate-x-0 absolute bottom-2 right-2'
+				"
+				@click.stop="myWorkspaceStore.favorite(board.id_str)"
+			/>
+		</NuxtLink>
+	</div>
 </template>
 
 <script lang="ts" setup>

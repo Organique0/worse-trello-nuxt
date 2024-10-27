a53d0e58904f0612081b30176448b3742ae85580
Luka Grabnar <grabnar.luka@gmail.com>
2024-10-27 14:32:34 +0100

    ok4

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -4,8 +4,10 @@ export default defineNuxtPlugin(async () => {
   const store = useMyUserStore();
   //if (user !== null) return;
 
-  const r = await useAsyncData('myUserStore', () => store.refresh());
+  console.log(store.user);
+  const r = await useAsyncData('myUserStore', () => store.fetchCurrentUser());
   console.log(r.status.value);
+  console.log(store.user);
   if (r.status.value != 'success') {
     console.log('User not found, redirecting to login from plugin auth');
     navigateTo('/login', { replace: true });
